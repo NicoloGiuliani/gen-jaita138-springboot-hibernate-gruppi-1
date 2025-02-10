5c2c1c48ecd48d7522c80a56570912a1474dbae9
Alexander <alexanderiba005@gmail.com>
2025-02-10 17:21:57 +0100

    Application

--- src/main/java/org/generation/jaita138/esercitazione/EsercitazioneApplication.java
+++ src/main/java/org/generation/jaita138/esercitazione/EsercitazioneApplication.java
@@ -1,11 +1,23 @@
 package org.generation.jaita138.esercitazione;
 
+import org.generation.jaita138.esercitazione.db.service.AutoreService;
+import org.generation.jaita138.esercitazione.db.service.GenereService;
+import org.generation.jaita138.esercitazione.db.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EsercitazioneApplication implements CommandLineRunner {
+	@Autowired
+	private LibroService libroService;
+
+	@Autowired
+	private GenereService genereService;
+
+	@Autowired
+	private AutoreService autoreService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EsercitazioneApplication.class, args);
@@ -13,7 +25,9 @@ public class EsercitazioneApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello world!!");
+		new CliManager(libroService, genereService, autoreService);
 	}
+
+	
 
 }
